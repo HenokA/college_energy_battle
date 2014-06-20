288f1b17574f6ceb759cdff3d231d5582351578c
Henok Addis <henok4@gmail.com>
2014-06-20 16:33:49 -0400

    Finished making the tsv file to hold a sorted chart of monthly percent change of energy usage.

--- separatedvalue.rb
+++ separatedvalue.rb
@@ -10,17 +10,33 @@ File.open('adjusted.txt', 'r'){ |file|
 	file.each_line do |line|
 		if colleges.index(line.strip.chomp.to_s)!=nil
 			#Store the college value
-			data[line.to_s] = Array.new
-			currentCollege = line.to_s
+			data[line.strip.chomp.to_s] = Array.new
+			currentCollege = line.strip.chomp.to_s
 			counter = 0
 		else
 			#look for the Common value
 			if counter !=2
 				line = line.split(',')
-				data[currentCollege].push(line[2])
+				data[currentCollege].push(line[2].strip.chomp.to_s)
 				counter +=1
 			end
 		end
 	end
 }
-puts data
+pushing = 0
+finaldata = Array.new
+#this finds the percent change of the current month vs the previous month
+data.each do |key, value|
+	pushing = (value[1].to_f - value[0].to_f).to_f / value[1].to_f
+	pushing = pushing*100
+	finaldata.push([key.to_s, pushing.to_s])
+end
+finaldata.sort_by!{|i| i[1]}
+puts finaldata
+#this writes the data into a tab separated file 
+File.open('data.tsv', 'w'){ |file|
+	file.write("college\tvalue\n")
+	finaldata.each do |key, value|
+		file.write(key.to_s + "\t" + value.to_s + "\n")
+	end
+}
