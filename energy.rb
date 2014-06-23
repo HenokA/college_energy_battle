e26727796fc51b37b63723e28244692c922383a9
Henok Addis <henok4@gmail.com>
2014-06-23 05:13:11 -0400

    Added some things to the about page to describe the numbers we used

--- energy.rb
+++ energy.rb
@@ -33,46 +33,36 @@ get '/' do
 		end
 	}
 	data.sort!{|x,y| y.to_f<=>x.to_f}
-	# puts data.to_s+"lol"
 	negdata.unshift("Worst Colleges")
 	posdata.unshift("Best Colleges")
-	puts posdata
 	college.shift
 	college.reverse!
-	puts college
 	erb :"index", :locals =>{:negdata => negdata, :posdata => posdata, :data => data, :college => college}
 end	
 
 get '/about' do
-	negdata = Array.new
-	posdata = Array.new
-	college = Array.new
-	File.open('data.tsv', 'r'){ |file|
-		file.each_line do |line|
-							line= line.split("\t")
-			if negdata == []
-				#this is what gives the dataset its name
-				negdata.push("Negative Values")
-			end
-			if posdata == []
-				posdata.push("Positive Values")#same as above
-			end
-			#in theory, there should never be a case where there is a 0% 
-			#change in energy because the chance of them using the EXACT 
-			#SAME AMOUNT is almost 0
-			if line[1].to_f <0 
-				negdata.push(line[1].to_f.round(3)) #pushes values less than 0
-			elsif line[1].to_f >0
-				posdata.push(line[1].to_f.round(3)) #pushes values greater than 0
-			end
-			college.push(line[0])
-		end
-	}
-	posdata.sort!{|x,y| y.to_f<=>x.to_f}
-	puts posdata
-	negdata.unshift("Worst Colleges")
-	posdata.unshift("Best Colleges")
-	erb :"about", :locals =>{:negdata => negdata, :posdata => posdata, :college => college}
+	# title = ""
+	# data = {'BERKELEY COLLEGE' =>[], 'BRANFORD COLLEGE' =>[], 
+	# 'CALHOUN COLLEGE,JOHN'=>[], 'DAVENPORT COLLEGE'=>[], 
+	# 'EZRA STILES COLLEGE'=>[], 'JONATHAN EDWARDS COL'=>[], 
+	# 'MORSE COLLEGE'=>[], 'PIERSON COLLEGE'=>[], 'SAYBROOK COLLEGE'=>[],
+	# 'SILLIMAN COLLEGE'=>[], 'TIMOTHY DWIGHT COLL.'=>[], 'TRUMBULL COLLEGE'=>[]}
+	# File.open('adjusted.txt', 'r'){ |file|
+	# 	file.each_line do |line|
+	# 		puts line
+	# 		line = line.split(",")
+	# 		puts line[0].strip.chomp.to_s != nil
+	# 		if data.index(line[0].strip.chomp.to_s) != nil
+	# 			title = line.strip.chomp.to_s
+	# 			puts title.to_s + 'AAHAAKJLSKJDLKSJDLKJALKJDF'
+	# 		elsif line[0].strip.chomp.to_s != 'newline'
+	# 			data[title].push(line[2].to_s)
+	# 		end
+				
+	# 	end
+	# }
+	# puts data
+	erb :"about"
 end
 
 get '/leaderboard' do
@@ -83,7 +73,6 @@ end
 
 
 get '/data/:name' do
-	puts "PENIS"
 	finaldates = Array.new
 	grab = false
 	dates = Array.new
